be69b10f166fe31710b82dfaf4f36a03f11caa2c
Maykon L Fonseca <maykonlfonseca@gmail.com>
2017-10-16 08:59:14 -0200

    Configura service ReceitaWS comm retrofit

---
+++ src/main/java/edu/com/unipam/maykon/services/ReceitaWSService.java
@@ -0,0 +1,11 @@
+package edu.com.unipam.maykon.services;
+
+import edu.com.unipam.maykon.entidades.CNPJ;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ReceitaWSService {
+    @GET("v1/cnpj/{cnpj}")
+    Call<CNPJ> getCNPJInfo(@Path("cnpj") String cnpj);
+}
